82e0fb583db098a7622d5cfb22ace10bf16c334e
RDBinns <rdpbinns@gmail.com>
2014-02-06 17:40:36 +0000

    Update csv2json.py

--- csv2json.py
+++ csv2json.py
@@ -4,6 +4,7 @@ import json
 csvfile = open('ConsentMap.csv', 'r')
 jsonfile = open('consentmap.json', 'w')
 
+# create fieldnames corresponding to the column headers on the spreadsheet
 fieldnames = ("jurisdiction","legislation","legislationURL","policyReq","tosReq","purposeReq","contactReq","addressReq","dntReq")
 reader = csv.DictReader( csvfile, fieldnames)
 for row in reader:
